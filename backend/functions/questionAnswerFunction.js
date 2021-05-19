2c6e9aee91645b7c343ea34fbb4b0924c59de070
Kseniia Anishchenko <kseninavoi@gmail.com>
2021-05-19 11:21:24 -0700

    updated logic for lambda function answer

--- backend/functions/questionAnswerFunction.js
+++ backend/functions/questionAnswerFunction.js
@@ -1,9 +1,9 @@
 const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
 
-    const response = {
+    let response = {
         statusCode: 501,
         headers: {
             "Content-Type": "text/plain",
@@ -29,30 +29,60 @@ exports.handler = (event, context, callback) => {
         }
     };
 
-    dynamoDb.get(params, (error, result) => {
-        try {    
-            if (error) {
-                console.error(error);
-                return;
-            }
+    let paramsForScore = {
+            TableName: process.env.DYNAMODB_TABLE,
+            Key: {
+                "primary_key": `scores`,
+                "secondary_key": `trivia/${triviaId}/kseniia`
+        }
+    };
 
-            console.log(params);
-            console.log(userAnswers);
+    try {
+        const getQuestionResp = await dynamoDb.get(params).promise();
 
-            if (!result.Item) {
-                response.statusCode = 404;
-                response.body = 'The question was not found';
-                return;
-            }
-            else {
-                response.headers["Content-Type"] = "application/json";
-                let correctAnswers = JSON.stringify(result.Item.record[questionId].correct);
-                response.body = correctAnswers;
-                response.statusCode = 200;
+        if (!getQuestionResp.Item) {
+            response.statusCode = 404;
+            response.body = 'The question was not found';
+            return;
+        }
+
+        response.headers["Content-Type"] = "application/json";
+        let correctAnswers = JSON.stringify(getQuestionResp.Item.record[questionId].correct);
+        response.body = correctAnswers;
+        response.statusCode = 200;
+
+        // logic for score
+        const userScoreResp = await dynamoDb.get(paramsForScore).promise();
+
+        //create a record in db if there is no one
+        if (!userScoreResp) {
+            console.log("No such record");
+        }
+
+        if (userScoreResp) {
+            let score = userScoreResp.Item;
+            let isCorrectAnswer;
+
+            if (score) {
+                if(correctAnswers.length === userAnswers.length) {
+                    isCorrectAnswer = correctAnswers.every(value => userAnswers.includes(value));
+                }
+
+                if (isCorrectAnswer) {
+                    //increase score by 1
+                    score = score + 1;
+
+                    console.log(correctAnswers);
+                    console.log(userAnswers);
+                    console.log(score);
+                }
             }
         }
-        finally {
-            callback(null, response);
-        }
-    });
+        
+    } catch (err) {
+        console.error(err);
+        response.statusCode = 501;
+    }    
+
+    return response;
 };
